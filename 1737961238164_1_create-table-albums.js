d81b99a25af31a5487dd899276030a6c479cdf17
lerningdif <yatnia@gmail.com>
2025-03-03 16:49:08 +0700

    Add files via upload

---
+++ 1737961238164_1_create-table-albums.js
@@ -0,0 +1,34 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('albums', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    coverUrl: {
+      type: 'TEXT',
+      notNull: false,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('albums');
+};

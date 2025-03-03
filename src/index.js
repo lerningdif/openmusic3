6a5d06df83f391a96984f1363d3b0401fefd31c5
lerningdif <yatnia@gmail.com>
2025-03-03 16:58:41 +0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const mapAlbumToDBModel = ({
+  id, name, year, created_at: createdAt, updated_at: updatedAt,
+}) => ({
+  id, name, year, createdAt, updatedAt,
+});
+
+const mapSongToDBModel = ({
+  id, title, year, genre, performer, duration, albumId, created_at: createdAt,
+  updated_at: updatedAt,
+}) => ({
+  id, title, year, genre, performer, duration, albumId, createdAt, updatedAt,
+});
+
+module.exports = { mapAlbumToDBModel, mapSongToDBModel };

59cd7b789c5eabfdd4632e0f8a94cd2e33190399
lerningdif <yatnia@gmail.com>
2025-03-05 05:08:33 +0700

    Update handler.js

--- src/api/uploads/handler.js
+++ src/api/uploads/handler.js
@@ -14,7 +14,7 @@ class UploadsHandler {
     const { id: albumId } = request.params;
     this._validator.validateImageHeaders(cover.hapi.headers);
  
-    const filename = await this._service.writeFile(cover, cover.hapi);
+    const filename = await this._storageService.writeFile(cover, cover.hapi);
     const url = `http://${process.env.HOST}:${process.env.PORT}/uploads/images/${filename}`;
     await this._albumsService.editAlbumCoverById(albumId, url);
  
